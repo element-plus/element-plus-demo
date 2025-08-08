65d4b75682a8b13ca664b00e91e9f286df08c1bd
warmthsea <2586244885@qq.com>
2025-08-08 17:44:05 +0800

    chore: update settings

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,11 +3,16 @@ defineOptions({
   name: 'IndexPage',
 })
 
-const name = ref('')
+const name = ref('Element Plus Demo')
 </script>
 
 <template>
-  <div class="w-screen h-screen flex items-center justify-center">
-    <el-input v-model="name" class="mt-10 !w-60" placeholder="please input" />
+  <div class="w-screen h-screen flex flex-col items-center justify-center">
+    <h1 class="text-lg mb-4">
+      {{ name }}
+    </h1>
+    <div>
+      <el-input v-model="name" size="large" clearable class="!w-60" placeholder="please input" />
+    </div>
   </div>
 </template>
